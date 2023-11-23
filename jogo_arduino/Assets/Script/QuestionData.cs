cac18948596b2bb91184bc2d73e0e7736d0346fd
BiancaPatrocinio7 <biancapatrocinio774@gmail.com>
2023-11-23 00:18:00 -0300

    Base do quiz funcionando, verifica se acertou ou nao

---
+++ jogo_arduino/Assets/Script/QuestionData.cs
@@ -0,0 +1,17 @@
+// This script contains the information for a trivia question
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Question", menuName = "ScriptableObjects/Question", order = 1)]
+public class QuestionData : ScriptableObject
+{
+    
+    public string question;
+    public int correctAnswer;
+    [Tooltip("The correct answer should always be listed first, they are ranzomized later")]
+    public string[] answers;
+    public Sprite[] images;
+    
+}
---
+++ jogo_arduino/Assets/Script/QuestionSetup.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestionSetup : MonoBehaviour
+{
+    [SerializeField]
+    private List<QuestionData> questions;
+    private QuestionData currentQuestion;
+
+    [SerializeField]
+    private TextMeshProUGUI questionText;
+    
+    [SerializeField]
+    private AnswerButton[] answerButtons;
+
+    [SerializeField]
+    private TextMeshProUGUI pointsTela;
+    
+
+
+    private void Awake()
+    {
+        // Get all the questions ready
+        GetQuestionAssets();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 3; i++){
+            //Get a new question
+            SelectNewQuestion();
+            // Set all text and values on screen
+            SetQuestionValues();
+            // Set all of the answer buttons text and correct answer values
+            SetAnswerValues();
+            SetPoint(i);
+            
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetQuestionAssets()
+    {
+        // Get all of the questions from the questions folder
+        questions = new List<QuestionData>(Resources.LoadAll<QuestionData>("Fase1"));
+    }
+
+    private void SelectNewQuestion()
+    {
+        // Get a random value for which question to choose
+        int randomQuestionIndex = Random.Range(0, questions.Count);
+        //Set the question to the randon index
+        currentQuestion = questions[randomQuestionIndex];
+        //Set the image to help to interpretate the question
+        
+        // Remove this questionm from the list so it will not be repeared (until the game is restarted)
+        questions.RemoveAt(randomQuestionIndex);
+    }
+
+    private void SetQuestionValues()
+    {
+        // Set the question text
+        questionText.text = currentQuestion.question;
+    }
+
+    private void SetAnswerValues()
+    {
+        // Randomize the answer button order
+       // List<string> answers = RandomizeAnswers(new List<string>(currentQuestion.answers));
+       int contadorCorrect = 0;
+
+        // Set up the answer buttons
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            // Create a temporary boolean to pass to the buttons
+            bool isCorrect = false;
+
+            // If it is the correct answer, set the bool to true
+            if(i == currentQuestion.correctAnswer)
+            {
+                isCorrect = true;
+                contadorCorrect++;
+                
+            }
+
+            answerButtons[i].SetIsCorrect(isCorrect);
+            answerButtons[i].SetAnswerText(currentQuestion.answers[i]);
+            answerButtons[i].SetAnswerImage(currentQuestion.images[i]);
+            
+        }
+    }
+
+    
+
+    public void SetPoint(int number){
+        string text = pointsTela + "/5";
+        pointsTela.text = text;
+    }
+}
