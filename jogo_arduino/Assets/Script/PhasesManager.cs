8bb1ac14ee8e0bd34c9583b44613021cbf016e13
Bibia Saturno <biancapatrocinio774@gmail.com>
2024-05-19 19:58:37 -0300

    changes scene

--- jogo_arduino/Assets/Script/PhasesManager.cs
+++ jogo_arduino/Assets/Script/PhasesManager.cs
@@ -10,13 +10,19 @@ public class PhasesManager : MonoBehaviour
     [Header("Components")]
     public Image phase1to2; // Image da UI a ser alterada
     public Image phase2to3;
+    public Image phase2;
     public Sprite cabo1;
     public Sprite cabo2;
+    public Sprite newPhase2;
+    public Button Botao;
+    
     private void Awake() 
     {
-
+        Botao.interactable = false;
         if(PlayerPrefs.GetInt("Phase2", 0) == 1){
+            Botao.interactable = true;
             phase1to2.sprite = cabo1;
+            phase2.sprite =  newPhase2;
         }
         if(PlayerPrefs.GetInt("Phase3", 0) == 1){
             phase2to3.sprite = cabo2;
@@ -24,9 +30,10 @@ public class PhasesManager : MonoBehaviour
         
 
     }
-    void Start()
-    {
-        
+    
+   void Start () {
+    
+ 
     }
 
     // Update is called once per frame
